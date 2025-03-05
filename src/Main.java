9d80e4b8e604eaef2dc92e140b18dbb1c3334dd2
Андрей <adlerSt@yandex.ru>
2025-03-05 19:58:00 +0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int[] array = {2 ,15, 0 ,3, 5, 12, 6, 8, 16, 7, 6, 6, 10, 1};
+        int[] arrayQuick = {4 ,15, 1 ,3, -5, 12, 6, 8, 16, 7, 19, 6, 10, 1};
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //Вызов сортировки пузырьком
+        Sort.bubbleSort(array);
+
+        System.out.println("-----------------------------------------------------------------------");
+
+        //Вызов быстрой сортировки
+        Sort.quickSort(arrayQuick);
+        if (arrayQuick != null && arrayQuick.length > 1) {
+            System.out.println("Быстрая сортировка.");
+            System.out.println("Отсортированный массив: " + Arrays.toString(arrayQuick));
+        } else {
+            System.out.println("Массив не нуждается в сортировке.");
         }
     }
 }
--- src/Sort.java
+++ src/Sort.java
@@ -1,3 +1,72 @@
+import java.util.Arrays;
+
 public class Sort {
-    
+    public static void bubbleSort(int[] ArrayName) {
+        int temp = 0;
+        long startTimeBubble = System.nanoTime();
+
+        for (int i = 0; i < ArrayName.length - 1; i++) {
+            for (int j = 0; j < ArrayName.length - 1; j++) {
+                if (ArrayName[j] > ArrayName[j + 1]) {
+                    temp = ArrayName[j];
+                    ArrayName[j] = ArrayName[j + 1];
+                    ArrayName[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println();
+        System.out.println("Сортировка пузырьком: " + Arrays.toString(ArrayName));
+        long endTimeBubble = System.nanoTime();
+        long sumTime = endTimeBubble - startTimeBubble;
+        System.out.println("Время выполнения сортировки: " + sumTime + " наносекунд. Или " +
+                (double)sumTime / 1_000_000 + " милисекунд.");
+    }
+
+    public static void quickSort(int[] ArrayName) {
+        if (ArrayName.length <= 1) {
+            return;
+        }
+
+        int pivot = ArrayName[ArrayName.length - 1];
+        int countOver = 0;
+        int countUnder = 0;
+
+        for (int i = 0; i < ArrayName.length - 1; i++) {
+            if (ArrayName[i] > pivot) {
+                countOver++;
+            } else {
+                countUnder++;
+            }
+        }
+        int[] ArrayUnder = new int[countUnder];
+        int[] ArrayOver = new int[countOver];
+
+        int indexOver = 0;
+        int indexUnder = 0;
+
+        for (int j = 0; j < ArrayName.length - 1; j++) {
+            if (ArrayName[j] > pivot) {
+                ArrayOver[indexOver] = ArrayName[j];
+                indexOver++;
+            } else {
+                ArrayUnder[indexUnder] = ArrayName[j];
+                indexUnder++;
+            }
+        }
+        quickSort(ArrayUnder);
+        quickSort(ArrayOver);
+
+        int n = 0;
+        for(int i = 0; i < ArrayUnder.length; i++) {
+            ArrayName[n] = ArrayUnder[i];
+            n++;
+        }
+        ArrayName[n] = pivot;
+        n++;
+        for(int i = 0; i < ArrayOver.length; i++) {
+            ArrayName[n] = ArrayOver[i];
+            n++;
+        }
+        //System.out.println("Массив после сортировки: " + Arrays.toString(ArrayName));
+    }
 }
